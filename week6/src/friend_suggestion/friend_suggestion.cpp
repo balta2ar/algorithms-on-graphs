f44fcb17257fc754a87e0a6abacf8d82d33c064d
Yuri Bochkarev <baltazar.bz@gmail.com>
2016-12-09 17:16:31 +0300

    week6: fix segfault in unittests

--- week6/src/friend_suggestion/friend_suggestion.cpp
+++ week6/src/friend_suggestion/friend_suggestion.cpp
@@ -7,6 +7,8 @@
 #include <utility>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
@@ -15,7 +17,7 @@ using namespace std;
 typedef vector<vector<vector<int>>> Adj;
 
 // Distances can grow out of int type
-typedef unsigned long long Len;
+typedef long long Len;
 
 // Vector of two priority queues - for forward and backward searches.
 // Each priority queue stores the closest unprocessed node in its head.
@@ -23,12 +25,23 @@ typedef vector<priority_queue<pair<Len, int>,
                               vector<pair<Len, int>>,
                               greater<pair<Len, int>>>> Queue;
 
-const Len INFINITY = numeric_limits<Len>::max();
+//const Len VERY_LARGE = numeric_limits<Len>::max() / 4;
+//const Len VERY_LARGE = 2305843009213693952; // 9223372036854775807 / 4;
+//const Len VERY_LARGE = numeric_limits<Len>::max() / 4;
+const Len VERY_LARGE = numeric_limits<Len>::max() / 4LL;
 
 ostream& operator<<(ostream& os, const pair<Len, int> p) {
     os << "(len=" << p.first << ",node=" << p.second << ")";
     return os;
 }
+
+// ostream& operator<<(ostream& os, const vector<int> v) {
+//     //os << "(len=" << p.first << ",node=" << p.second << ")";
+//     for (auto x : v) {
+//         cout << x << " ";
+//     };
+//     return os;
+// }
 
 class Bidijkstra {
     // Number of nodes
@@ -46,10 +59,12 @@ class Bidijkstra {
     // and distance_[1] stores distances for the backward search.
     vector<vector<Len>> distance_;
     // Stores all the nodes visited either by forward or backward search.
-    vector<int> workset_;
+    //vector<int> workset_;
+    unordered_set<int> workset_;
     // Stores a flag for each node which is True iff the node was visited
     // either by forward or backward search.
-    vector<bool> visited_;
+    //vector<bool> visited_;
+    vector<vector<bool>> visited_;
 
     vector<vector<int>> parent_;
 
@@ -59,8 +74,9 @@ public:
           m_(m),
           adj_(adj),
           cost_(cost),
-          distance_(2, vector<Len>(n, INFINITY)),
-          visited_(n),
+          distance_(2, vector<Len>(n, VERY_LARGE)),
+          visited_(2, vector<bool>(n, false)),
+          //visited_(n),
           parent_(2, vector<int>(n, -1))
     {
         workset_.reserve(n);
@@ -88,13 +104,18 @@ public:
     // clear the changes made by the previous query.
     void clear() {
 
-        for (size_t i = 0; i < workset_.size(); ++i) {
-            int v = workset_[i];
-            distance_[0][v] = distance_[1][v] = INFINITY;
-            visited_[v] = false;
+        //for (size_t i = 0; i < workset_.size(); ++i) {
+        //for (size_t i = 0; i < n_; ++i) {
+        for (int v : workset_) {
+            //int v = workset_[i];
+            //int v = i;
+            distance_[0][v] = distance_[1][v] = VERY_LARGE;
+            //visited_[v] = false;
+            visited_[0][v] = visited_[1][v] = false;
             parent_[0][v] = parent_[1][v] = -1;
         }
         workset_.clear();
+        workset_.reserve(n_);
     }
 
     Len backtrack(int source, int target) {
@@ -142,29 +163,154 @@ public:
     //     #endif // MAIN
     // }
 
-    void relax(Queue &front, int u, int v, int v_index) {
-        Len alt = distance_[0][u] + cost_[0][u][v_index];
-
-        // if (distance_[0][v] == INFINITY) {
-        //     distance_[0][v] = alt;
-        //     parent_[0][v] = u;
-        //     front[0].push({alt, v});
-        // } else {
-        //     distance_[0][v] = min(distance_[0][v], alt);
-        // }
-
-        if (alt < distance_[0][v]) {
-            distance_[0][v] = alt;
-            parent_[0][v] = u;
-
-            front[0].push({alt, v});
-        }
-
-        //front[0].push({alt, v});
-    }
+    // void relax(Queue &front, int u, int v, int v_index) {
+    //     Len alt = distance_[0][u] + cost_[0][u][v_index];
+    //
+    //     // if (distance_[0][v] == VERY_LARGE) {
+    //     //     distance_[0][v] = alt;
+    //     //     parent_[0][v] = u;
+    //     //     front[0].push({alt, v});
+    //     // } else {
+    //     //     distance_[0][v] = min(distance_[0][v], alt);
+    //     // }
+    //
+    //     if (alt < distance_[0][v]) {
+    //         distance_[0][v] = alt;
+    //         parent_[0][v] = u;
+    //
+    //         front[0].push({alt, v});
+    //     }
+    //
+    //     //front[0].push({alt, v});
+    // }
 
     Len query(int source, int target) {
-        return query_onedirectional(source, target);
+        //return query_onedirectional(source, target);
+        return query_bidirectional(source, target);
+    }
+
+    int extract_min(Queue& front, int side) {
+        if (front[side].empty()) {
+            return -1;
+        }
+
+        pair<Len, int> node = front[side].top();
+        front[side].pop();
+        return node.second;
+    }
+
+    void process(Queue& front, int side, int u) {
+        auto neighbors = adj_[side][u];
+        for (size_t v_index = 0; v_index < neighbors.size(); v_index++) {
+            int v = neighbors[v_index];
+            Len alt = distance_[side][u] + cost_[side][u][v_index];
+
+            if (alt < distance_[side][v]) {
+                distance_[side][v] = alt;
+                parent_[side][v] = u;
+
+                front[side].push({alt, v});
+                //workset_.push_back(v);
+                workset_.insert(v);
+            }
+        }
+
+        //cout << "side " << side << " visited " << u
+        //    << " parent " << parent_[side][u] << endl;
+        visited_[side][u] = true;
+        //workset_.push_back(u);
+        workset_.insert(u);
+        #ifdef MAIN
+        //cerr << u << endl;
+        #endif // MAIN
+    }
+
+    bool do_iteration(Queue& front, int side, int source, int target, Len& dist) {
+        int u = extract_min(front, side);
+        if (u == -1) {
+            // cout << "Empty front on side " << side << endl;
+            return false;
+        }
+        process(front, side, u);
+
+        int other_side = 1 - side;
+        //if (distance_[other_side][u] != VERY_LARGE) {
+        if (visited_[other_side][u]) {
+            // cout << "DIST on other side " << other_side << " to u " << u
+            //     << " is " << distance_[other_side][u]
+            //     << " parent " << parent_[side][u] << endl;
+            dist = shortest_path(source, target);
+            return true;
+        }
+        return false;
+    }
+
+    int shortest_path(int source, int target) {
+        // cout << "SHORTEST REACHED" << endl;
+
+        Len dist = VERY_LARGE;
+        int u_best = -1;
+
+        // cout << "Finding best dist" << endl;
+        for (int u : workset_) {
+            Len candidate_dist = distance_[0][u] + distance_[1][u];
+            // cout << "Cand dist for " << u << " = " << candidate_dist
+            //     << " | " << distance_[0][u] << " " << distance_[1][u] << endl;
+            if (candidate_dist < dist) {
+                u_best = u;
+                dist = candidate_dist;
+            }
+        }
+        // cout << "best dist " << dist << " to u_best " << u_best << endl;
+
+        vector<int> path;
+        int last = u_best;
+
+        // cout << "Building path from source" << endl;
+        while (last != source) {
+            // cout << last << endl;
+            path.push_back(last);
+            last = parent_[0][last];
+        }
+        reverse(path.begin(), path.end());
+
+        // cout << "Building path from target" << endl;
+        last = u_best;
+        while (last != target) {
+            // cout << last << endl;
+            last = parent_[1][last];
+            path.push_back(last);
+        }
+
+        // cout << "PATH " << path << endl;
+        return dist;
+    }
+
+    Len query_bidirectional(int source, int target) {
+        clear();
+        Queue front(2);
+        distance_[0][source] = distance_[1][target] = 0;
+
+        front[0].push({0, source});
+        front[1].push({0, target});
+
+        //while (!front[0].empty()) {
+        //while (true) {
+        Len dist = -1;
+        while (!front[0].empty() || !front[1].empty()) {
+            if (do_iteration(front, 0, source, target, dist)) return dist;
+            if (do_iteration(front, 1, source, target, dist)) return dist;
+        }
+
+        //visit(front, 0, source, 0);
+        //visit(front, 1, target, 0);
+        // Implement the rest of the algorithm yourself
+
+        // if (distance_[0][target] != VERY_LARGE) {
+        //     return backtrack(source, target);
+        // }
+        return -1;
+
     }
 
     // Returns the distance from s to t in the graph.
@@ -193,8 +339,8 @@ public:
                 }
             }
 
-            visited_[u] = true;
-            workset_.push_back(u);
+            visited_[0][u] = true;
+            workset_.insert(u);
             #ifdef MAIN
             cerr << u << endl;
             #endif // MAIN
@@ -204,7 +350,7 @@ public:
         //visit(front, 1, target, 0);
         // Implement the rest of the algorithm yourself
 
-        if (distance_[0][target] != INFINITY) {
+        if (distance_[0][target] != VERY_LARGE) {
             return backtrack(source, target);
         }
         return -1;
@@ -273,8 +419,9 @@ Bidijkstra readFromFile(FILE *file) {
     Adj adj(2, vector<vector<int>>(n));
     Adj cost(2, vector<vector<int>>(n));
     for (int i=0; i<m; ++i) {
-        int u, v, c;
-        fscanf(file, "%d%d%d", &u, &v, &c);
+        int u, v;
+        Len c;
+        fscanf(file, "%d%d%lld", &u, &v, &c);
         adj[0][u-1].push_back(v-1);
         cost[0][u-1].push_back(c);
         adj[1][v-1].push_back(u-1);
