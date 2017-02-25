614860007d6bc013582f97d59ffc8af9f20d9553
Yuri Bochkarev <baltazar.bz@gmail.com>
2017-02-25 21:13:03 +0300

    week6: implement one-directional Dijkstra on python3

--- week6/src/friend_suggestion/friend_suggestion.cpp
+++ week6/src/friend_suggestion/friend_suggestion.cpp
@@ -223,7 +223,7 @@ public:
     }
 
     void process(Queue& front, int side, int u, int source, int target) {
-        cout << "PROCESSING u " << u << " side " << side << endl;
+        //cout << "PROCESSING u " << u << " side " << side << endl;
 
         // process all u's neighbors
         auto neighbors = adj_[side][u];
@@ -235,16 +235,16 @@ public:
             Len actual_dist = distance_[side][u] +
                 edge_cost(u, v, v_index, source, target, side);
 
-            cout << "side " << side << " u " << u << " v " << v
-                << " actual_dist " << actual_dist
-                << " dist[v] " << distance_[side][v]
-                << endl;
+            // cout << "side " << side << " u " << u << " v " << v
+            //     << " actual_dist " << actual_dist
+            //     << " dist[v] " << distance_[side][v]
+            //     << endl;
 
             // is this a better path to v?
             if (actual_dist < distance_[side][v]) {
-                cout << "new shorter dist: " << actual_dist
-                    << " < " << distance_[side][v]
-                    << endl;
+                // cout << "new shorter dist: " << actual_dist
+                //     << " < " << distance_[side][v]
+                //     << endl;
                 distance_[side][v] = actual_dist;
                 parent_[side][v] = u;
 
@@ -263,16 +263,16 @@ public:
                 //Len new_best_path_len = cost_[side][u][v_index] +
                 Len new_best_path_len = edge_cost(u, v, v_index, source, target, side) +
                     distance_[side][u] + distance_[other_side][v];
-                cout << "new_best_path_len " << new_best_path_len << endl;
+                // cout << "new_best_path_len " << new_best_path_len << endl;
 
                 if (new_best_path_len < best_path_len_) {
-                    cout << "update best_path_len_" << endl;
+                    // cout << "update best_path_len_" << endl;
                     best_path_len_ = new_best_path_len;
                 }
             }
         }
 
-        cout << "done with u " << u << endl;
+        // cout << "done with u " << u << endl;
         visited_[side][u] = true;
         workset_.insert(u);
         log_to_file(visited_filename, u);
@@ -348,7 +348,7 @@ public:
         //print_coords(source, target);
         //return -1;
 
-        cout << "search from source " << source << " to target " << target << endl;
+        // cout << "search from source " << source << " to target " << target << endl;
 
         front[0].push({0, source});
         front[1].push({0, target});
@@ -441,10 +441,10 @@ public:
 
     virtual Len edge_cost(int u, int v, int v_index, int source, int target, int side) {
         Len result = cost_[side][u][v_index] + potential(u, v, source, target, side);
-        cout << "edge_cost u v " << u << " " << v << " = " << result << endl;
-        cout << "cost " << cost_[side][u][v_index] << " dist " << dist(u, v) << endl;
-        cout << "u " << xy_[u].first << ", " << xy_[u].second << endl;
-        cout << "v " << xy_[v].first << ", " << xy_[v].second << endl;
+        // cout << "edge_cost u v " << u << " " << v << " = " << result << endl;
+        // cout << "cost " << cost_[side][u][v_index] << " dist " << dist(u, v) << endl;
+        // cout << "u " << xy_[u].first << ", " << xy_[u].second << endl;
+        // cout << "v " << xy_[v].first << ", " << xy_[v].second << endl;
         assert(result >= 0);
         return result;
     }
@@ -470,8 +470,8 @@ public:
 
         result += delta;
         Len l = (Len) result;
-        cout << "potential u v "<< u << " " << v << " source target " << source << " " << target  << endl;
-        cout << "potential delta " << delta << " l " << l << endl;
+        // cout << "potential u v "<< u << " " << v << " source target " << source << " " << target  << endl;
+        // cout << "potential delta " << delta << " l " << l << endl;
         return l;
     }
 
@@ -479,7 +479,7 @@ public:
         int other_side = 1 - side;
 
         if (front[side].empty() || front[other_side].empty()) {
-            cout << "CAN_STOP false: either side is empty" << endl;
+            // cout << "CAN_STOP false: either side is empty" << endl;
             return false;
         }
 
@@ -495,8 +495,8 @@ public:
             p_f(target, source, target, side);
 
         bool result = front_best_path >= best_path_len_with_estimate;
-        cout << "CAN_STOP left " << front_best_path << " >= right " << best_path_len_with_estimate << endl;
-        cout << "CAN_STOP side " << side << " u " << u << " result " << result << endl;
+        // cout << "CAN_STOP left " << front_best_path << " >= right " << best_path_len_with_estimate << endl;
+        // cout << "CAN_STOP side " << side << " u " << u << " result " << result << endl;
         return result;
     }
 };
--- week6/src/friend_suggestion/friend_suggestion.py
+++ week6/src/friend_suggestion/friend_suggestion.py
@@ -1,53 +1,134 @@
 #!/usr/bin/python3
 
 import sys
-import queue
+from queue import PriorityQueue
 
-class BiDij:
+class DijkstraBidirectional:
     def __init__(self, n):
-        self.n = n;                             # Number of nodes
+        self.n = n                              # Number of nodes
         self.inf = n*10**6                      # All distances in the graph are smaller
-        self.d = [[self.inf]*n, [self.inf]*n]   # Initialize distances for forward and backward searches
+        self.dist = [[self.inf]*n, [self.inf]*n]   # Initialize distances for forward and backward searches
         self.visited = [False]*n                  # visited[v] == True iff v was visited by forward or backward search
         self.workset = []                       # All the nodes visited by forward or backward search
 
     def clear(self):
-    """Reinitialize the data structures for the next query after the previous query."""
+        """Reinitialize the data structures for the next query after the previous query."""
         for v in self.workset:
-            self.d[0][v] = self.d[1][v] = self.inf
-            self.visited[v] = False;
+            self.dist[0][v] = self.dist[1][v] = self.inf
+            self.visited[v] = False
         del self.workset[0:len(self.workset)]
 
     def visit(self, q, side, v, dist):
-    """Try to relax the distance to node v from direction side by value dist."""
+        """Try to relax the distance to node v from direction side by value dist."""
         # Implement this method yourself
         pass
 
-    def query(self, adj, cost, s, t):
+    def query(self, adj, cost, source, target):
         self.clear()
-        q = [queue.PriorityQueue(), queue.PriorityQueue()]
-        self.visit(q, 0, s, 0)
-        self.visit(q, 1, t, 0)
+        queue = [PriorityQueue(), PriorityQueue()]
+        self.visit(queue, 0, source, 0)
+        self.visit(queue, 1, target, 0)
         # Implement the rest of the algorithm yourself
         return -1
+
+class DijkstraOnedirectional:
+    def __init__(self, n, m):
+        self.n = n                              # Number of nodes
+        self.m = m
+        self.inf = n*10**6                      # All distances in the graph are smaller
+        self.dist = [[self.inf]*n, [self.inf]*n]   # Initialize distances for forward and backward searches
+        self.visited = [False]*n                  # visited[v] == True iff v was visited by forward or backward search
+        self.workset = []                       # All the nodes visited by forward or backward search
+        self.parent = [[None]*n, [None]*n]      # Used for backtracking
+
+        self.adj = None
+        self.cost = None
+
+    def clear(self):
+        """
+        Reinitialize the data structures for the next query after the previous query.
+        """
+        for v in self.workset:
+            self.dist[0][v] = self.dist[1][v] = self.inf
+            self.visited[v] = False
+        del self.workset[0:len(self.workset)]
+
+    def visit(self, queue, side, u):
+        """
+        Try to relax the distance to node u from direction side by value dist.
+        """
+        neighbors = self.adj[0][u]
+        for v_index, v in enumerate(neighbors):
+            alt = self.dist[0][u] + self.cost[0][u][v_index]
+
+            if alt < self.dist[0][v]:
+                self.dist[0][v] = alt
+                self.parent[0][v] = u
+
+                queue[0].put((alt, v))
+
+        self.visited[u] = True
+        self.workset.append(u)
+
+    def query(self, adj, cost, source, target):
+        self.clear()
+        queue = [PriorityQueue(), PriorityQueue()]
+
+        self.adj = adj
+        self.cost = cost
+
+        self.dist[0][source] = 0
+        queue[0].put((0, source))
+
+        while not queue[0].empty():
+            _, u = queue[0].get()
+            self.visit(queue, 0, u)
+
+        if self.dist[0][target] != self.inf:
+            return self.backtrack(source, target)
+
+        return -1
+
+    def backtrack(self, source, target):
+        # path = []
+        #
+        # current = target
+        # while current != source:
+        #     path.append(current)
+        #     current = self.parent[0][current]
+        # path.append(current)
+
+        #print(list(reversed(path)))
+        return self.dist[0][target]
 
 
 def readl():
     return map(int, sys.stdin.readline().split())
 
 
-if __name__ == '__main__':
-    n,m = readl()
+def read_from_stdin():
+    n, m = readl()
     adj = [[[] for _ in range(n)], [[] for _ in range(n)]]
     cost = [[[] for _ in range(n)], [[] for _ in range(n)]]
-    for e in range(m):
-        u,v,c = readl()
+    for _ in range(m):
+        u, v, c = readl()
         adj[0][u-1].append(v-1)
         cost[0][u-1].append(c)
         adj[1][v-1].append(u-1)
         cost[1][v-1].append(c)
+
+    return n, m, adj, cost
+
+
+def main():
+    n, m, adj, cost = read_from_stdin()
     t, = readl()
-    bidij = BiDij(n)
-    for i in range(t):
+
+    bidij = DijkstraOnedirectional(n, m)
+    for _ in range(t):
         s, t = readl()
         print(bidij.query(adj, cost, s-1, t-1))
+
+
+if __name__ == '__main__':
+    main()
